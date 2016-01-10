844a86ee7c3aa0970d7d480589058a62f3e1a368
ThomasRgbg <tom_p@gmx.de>
2016-01-10 22:45:31 +0100

    Next steps... still need to fix the default log file target

---
+++ heiz_ctrl/heizung_all_off.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import sys
+from time import sleep, strftime
+from datetime import datetime, timedelta
+
+import logging
+import argparse
+
+from heizctrl.ctrl import HeizungControl
+
+
+if __name__ == '__main__':
+
+    argparser = argparse.ArgumentParser()
+
+    # Output verbosity options.
+    argparser.add_argument('-q', '--quiet', help='set logging to ERROR',
+                     action='store_const', dest='loglevel',
+                     const=logging.ERROR, default=logging.INFO)
+    argparser.add_argument('-d', '--debug', help='set logging to DEBUG',
+                     action='store_const', dest='loglevel',
+                     const=logging.DEBUG, default=logging.INFO)
+    argparser.add_argument('-v', '--verbose', help='set logging to COMM',
+                     action='store_const', dest='loglevel',
+                     const=5, default=logging.INFO)
+
+    argparser.add_argument("-l", "--logfile", dest="logfile",
+                    help="logfile to use")
+
+    argparser.add_argument("-i", "--init", dest="init",
+                    help="Init hardware")
+
+
+
+    args = argparser.parse_args()
+
+    if args.logfile is not None:
+        if sys.version_info < (3, 0):
+            console_log = open(opts.logfile, 'a', 1)
+        else:
+            console_log = open(opts.logfile, 'a', 1, encoding='utf-8')
+        sys.stdout = console_log
+        sys.stderr = console_log
+
+    # Setup logging.
+    logging.basicConfig(level = args.loglevel, datefmt='%H:%M:%S', format='%(asctime)s %(levelname)s:%(name)s:%(funcName)s:%(message)s')
+    logger = logging.getLogger(__name__)
+
+    logger.debug('Used options: {0}'.format(args))
+
+    if args.init is not None:
+        init = True
+    else:
+        init = False
+
+    heizung = HeizungControl(init_board=init)
+    heizung.heizung_all_off()
+
+
