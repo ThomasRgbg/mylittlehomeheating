95978682799413ca0de589f41c7161cb9edcf389
ThomasRgbg <tom_p@gmx.de>
2016-01-10 15:36:19 +0100

    some reworks

--- heiz_ctrl/heizctrl/ctrl.py
+++ heiz_ctrl/heizctrl/ctrl.py
@@ -10,7 +10,8 @@ import pifacedigitalio
 from .cron import HeizungCronControl
 
 class HeizungControl(HeizungCronControl):
-
+    def __init__(self, init_board=False):
+        self.pdf = pifacedigitalio.PiFaceDigital(init_board=init_board)
 
     def heizung_create_token(self, channel, duration):
 
@@ -22,23 +23,22 @@ class HeizungControl(HeizungCronControl):
         file.flush()
         file.close()
 
+    def heizung_all_off(self):
+        self.pfd.output_pins[0].value=1 # "Schlafzimmer" (Buero)
+        self.pfd.output_pins[1].value=1 # "Kinderzimmer" (Schlafzimmer)
+        self.pfd.output_pins[2].value=1 # Bad
+        self.pfd.output_pins[3].value=1 # Wohnzimmer
+        sleep(0.1)
+        print("State of Piface: {:08b} ".format(self.pfd.output_port.value))
 
     def heizung_1ch_on(self, channel):
-        pfd = pifacedigitalio.PiFaceDigital()
-
-        pfd.output_pins[0].value=1 # "Schlafzimmer" (Buero)
-        pfd.output_pins[1].value=1 # "Kinderzimmer" (Schlafzimmer)
-        pfd.output_pins[2].value=1 # Bad
-        pfd.output_pins[3].value=1 # Wohnzimmer
-        sleep(0.1)
-        pfd.output_pins[channel].value=0
-
-        logger.info("State of Piface: {:08b} ".format(pfd.output_port.value))
+        self.heizung_all_off()
+        self.pfd.output_pins[channel].value=0
+        print("State of Piface: {:08b} ".format(self.pfd.output_port.value))
 
     def heizung_1ch(self, channel, duration):
         self.heizung_create_token(channel, duration)
         # self.heizung_1ch_on(channel)
-
         self.add_off_event(datetime.now(), channel, duration)
 
 
