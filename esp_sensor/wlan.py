6c3692260d80eee4a2eabd5e0107b4466a5adf68
ThomasRgbg <tom_p@gmx.de>
2017-01-27 19:52:17 +0100

    periodic ntp sync, since ESP8266 rtc drifts

--- esp_sensor/wlan.py
+++ esp_sensor/wlan.py
@@ -13,19 +13,22 @@ class mywlan(object):
         self.ap_if = network.WLAN(network.AP_IF)
         self.sta_if.active(False)
         self.ap_if.active(False)
+        self.synccount=0
 
     def connect(self):
+        start = time.ticks_ms()
+        print("Connect wlan")
         if not self.sta_if.isconnected():
             self.sta_if.active(True)
             self.sta_if.connect('thomasssid', password)
 
-        start = time.ticks_ms()
-        print("Connect wlan")
+        self.synccount += 1
         while (time.ticks_diff(time.ticks_ms(), start) < 5000):
-            if self.sta_if.isconnected() and time.time() < 536962532:
+            if self.sta_if.isconnected() and (time.time() < 536962532 or self.synccount > 50):
                 try:
                     print("Sync Time via NTP")
                     ntptime.settime()
+                    self.synccount = 0
                 except OSError:
                     # It nis not critical, if time is not synced, since 
                     # he will do this with next send loop.
